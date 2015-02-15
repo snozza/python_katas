4545f9c630c47afe2f17ae61f8eb7c24ee13d2a0
Andrew Snead <andrewcsnead@gmail.com>
2015-02-15 19:10:50 +0000

    separated out Position into own class

--- rover.py
+++ rover.py
@@ -1,16 +1,31 @@
 class Rover:
     
-    def __init__(self, x=0, y=0, direction='south'):
-        self.x = x
-        self.y = y
+    def __init__(self, position, direction='south'):
+        self.position = position
         self.direction = direction
 
     def move(self, commands=()):
         for cmd in commands:
             if cmd == 'f' and self.direction == 'south':
-                self.x -= 1
+                self.position.down()
             if cmd == 'b' and self.direction == 'south':
-                self.x += 1
+                self.position.up()
+
+    def get_position(self):
+        return self.position.get_position()
+
+class Position:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def down(self):
+        self.x -= 1
+
+    def up(self):
+        self.x += 1
 
     def get_position(self):
         return (self.x, self.y)
+
--- rover_test.py
+++ rover_test.py
@@ -1,32 +1,31 @@
-from rover import Rover
+from rover import Rover, Position
 import unittest
 
 class RoverTests(unittest.TestCase):
 
-#rover has a default x, y and direction upon instantiation
-    def test_new_rover_default_coordinates(self):
-        rover = Rover()
-        self.assertEqual(0, rover.x)
-        self.assertEqual(0, rover.y)
+#position has a default x, y
+    def test_position_default_coordinates(self):
+        position = Position()
+        self.assertEqual(0, position.x)
+        self.assertEqual(0, position.y)
 
 #rover can be given coords upon instantiation
     def test_rover_given_coordinates(self):
-        rover = Rover(12, 10)
-        self.assertEqual(12, rover.x)
-        self.assertEqual(10, rover.y)
+        rover = Rover(Position(12, 10), 'south')
+        self.assertEqual((12, 10), rover.get_position())
 
     def test_move_one_forward(self):
-        rover = Rover(0, 0, 'south')
+        rover = Rover(Position(0, 0), 'south')
         rover.move(('f'))
         self.assertEqual((-1, 0), rover.get_position())
 
     def test_move_three_forward(self):
-        rover = Rover(0, 0, 'south')
+        rover = Rover(Position(0, 0), 'south')
         rover.move(('f', 'f', 'f'))
         self.assertEqual((-3, 0), rover.get_position())
 
     def test_move_two_forward_one_backwards(self):
-        rover = Rover(0, 0, 'south')
+        rover = Rover(Position(0, 0), 'south')
         rover.move(('f', 'f', 'b'))
         self.assertEqual((-1, 0), rover.get_position())
 
