b462784f7d5a840bbe9977558d645ebdc018e211
Andrew Snead <andrewcsnead@gmail.com>
2015-02-15 21:49:22 +0000

    everything removed

--- rover.py
+++ rover.py
@@ -1,1 +1,2 @@
-
+class Rover():
+    pass
--- rover_test.py
+++ rover_test.py
@@ -1,16 +1,16 @@
-from rover import Rover, Position
+from rover import Rover
 import unittest
 
 class RoverTests(unittest.TestCase):
 
 #position has a default x, y
     def test_position_default_coordinates(self):
-
+        pass
 #rover can be given coords upon instantiation
     def test_rover_given_coordinates(self):
-
+        pass
     def test_move_one_forward(self):
-
+        pass
 if __name__ == '__main__':
     unittest.main()
 
