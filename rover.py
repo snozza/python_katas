5dd9eb0be824833df148e93462cb2c59c4be381a
Andrew Snead <andrewcsnead@gmail.com>
2015-02-15 14:37:16 +0000

    added init class

--- rover.py
+++ rover.py
@@ -1,6 +1,6 @@
 class Rover:
     
-    def __init__(self, x, y, orientation):
+    def __init__(self, x=0, y=0, orientation='N'):
         self.x = x
         self.y = y
         self.orientation = orientation 
--- rover_test.py
+++ rover_test.py
@@ -3,19 +3,17 @@ import unittest
 
 class RoverTests(unittest.TestCase):
 
-#rover has a x, y and origination upon initiation
-    def test_access_rover_variables(self):
-        r = rover.Rover(10, 12, 'N')
-        self.assertEqual(10, r.x)
-        self.assertEqual(12, r.y)
-        self.assertEqual('N', r.orientation)
+#rover has a default x, y and origination upon initiation
+    def test_new_rover_default_coordinates(self):
+        r = rover.Rover()
+        self.assertEqual(0, r.x)
+        self.assertEqual(0, r.y)
 
-#implement commands to move rover forwards and backwards
-    def test_rover_receives_forward_command(self):
-        r = rover.Rover(5, 10, 'N')
-        r.executeCommands(['f'])
-        self.assertEqual(5, r.x)
-        self.assertEqual(11, r.y)
+#rover can be given coords upon initiation
+    def test_rover_given_coordinates(self):
+        r = rover.Rover(12, 10)
+        self.assertEqual(12, r.x)
+        self.assertEqual(10, r.y)
 
 
 if __name__ == '__main__':
