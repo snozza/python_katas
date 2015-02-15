fa7b78e118bee777b9878b8de339722c58de7dbb
Andrew Snead <andrewcsnead@gmail.com>
2015-02-15 22:31:53 +0000

    removed commented out leftover code

--- rover_test.py
+++ rover_test.py
@@ -4,27 +4,18 @@ import unittest
 class RoverTests(unittest.TestCase):
 
     def test_move_one_forward(self):
-        # rover = Rover(Position(0, 0), Direction.S)
-        # rover.move(('f',))
-        # self.assertEqual(Position(-1, 0), rover.get_position())
         self._move_and_test_rover(Position(0, 0),
                 Direction.S,
                 ('f',),
                 Position(-1, 0))
 
     def test_move_one_forward_one_backward(self):
-        # rover = Rover(Position(0, 0), Direction.S)
-        # rover.move(('f', 'b'))
-        # self.assertEqual(Position(0, 0), rover.get_position())
         self._move_and_test_rover(Position(0, 0),
                 Direction.S,
                 ('f', 'b'),
                 Position(0, 0))
 
     def test_move_one_forward_two_backward_north(self):
-        # rover = Rover(Position(0, 0), Direction.N)
-        # rover.move(('f', 'b', 'b'))
-        # self.assertEqual(Position(-1, 0), rover.get_position())
         self._move_and_test_rover(Position(0, 0),
                 Direction.N,
                 ('f', 'b', 'b'),
